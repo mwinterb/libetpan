97fefd831103dacb0017867d8a06f22c38b399a4
Michael Winterberg <mike.winterberg@gmail.com>
2011-09-26 17:47:34 -0700

    Memory leak fix of peer certificate. Changed ssl_data_new_full signature to not lose constness.

--- src/data-types/mailstream_ssl.c
+++ src/data-types/mailstream_ssl.c
@@ -295,7 +295,7 @@ static int mailstream_openssl_client_cert_cb(SSL *ssl, X509 **x509, EVP_PKEY **p
 		return 0;
 }
 
-static struct mailstream_ssl_data * ssl_data_new_full(int fd, SSL_METHOD * method, void (* callback)(struct mailstream_ssl_context * ssl_context, void * cb_data), void * cb_data)
+static struct mailstream_ssl_data * ssl_data_new_full(int fd, const SSL_METHOD * method, void (* callback)(struct mailstream_ssl_context * ssl_context, void * cb_data), void * cb_data)
 {
   struct mailstream_ssl_data * ssl_data;
   SSL * ssl_conn;
@@ -930,6 +930,8 @@ ssize_t mailstream_ssl_get_certificate(mailstream *stream, unsigned char **cert_
   
   *cert_DER = NULL;
   len = (ssize_t) i2d_X509(cert, cert_DER);
+
+  X509_free(cert);
   
   return len;
 #else
